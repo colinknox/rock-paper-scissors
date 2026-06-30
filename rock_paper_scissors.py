300438f6afe90afc535eeb4698602710749154fc
ColinKnox <colinwknox@gmail.com>
2026-06-30 11:32:29 -0400

    finished my pseudocode

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,3 +1,27 @@
+# Tuple of valid answers and their respective emojis.
 # Ask user "Rock, paper, or scissors (r/p/s): "
-# If invalid input
-    # Print "Invalid choice!"
+# Loop
+    # If invalid input
+        # Print "Invalid choice!"
+    # If 
+        # user_choice == "r" and computer_choice == "s" or
+        # user_choice == "p" and computer_choice == "r" or
+        # user_choice == "s" and computer_choice == "p"
+        # Print user_choice
+        # Print computer_choice
+        # Print "You win!"
+    # If 
+        # user_choice == "r" and computer_choice == "p" or
+        # user_choice == "p" and computer_choice == "s" or
+        # user_choice == "s" and computer_choice == "r"
+        # Print user_choice
+        # Print computer_choice
+        # Print "You lose!"
+    # Else
+        # pass
+
+    # Prompt user with "Continue (y/n): "
+    # If user choices "y":
+        # Go back into the loop.
+    # Else:
+        # break
