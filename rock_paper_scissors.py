4ca962c04a998e2cb12d18182c6cfe2ce6f7a163
ColinKnox <colinwknox@gmail.com>
2026-07-01 12:30:18 -0400

    computer can now randomly generate 1 of 3 choices from the given dict.

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -7,6 +7,7 @@ possible_choices = {
 }
 
 user_choice = str(input("Rock, paper, or scissors? (r/p/s): ")).lower()    
+computer_choice = random.choice(list(possible_choices))
 
 if user_choice not in possible_choices:
     print("Invalid choice!")
@@ -14,9 +15,10 @@ if user_choice not in possible_choices:
 #     user_choice == "r" and computer_
 # ):
     
-
-
 print(f"USER CHOICE = {user_choice}")
+print(f"COMPUTER CHOICE = {computer_choice}")
+
+
 # Tuple of valid answers and their respective emojis.
 # Ask user "Rock, paper, or scissors (r/p/s): "
 # Loop
